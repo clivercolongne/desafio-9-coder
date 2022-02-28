4cd05a8ebabbeaad7556d8169398e7cbeef5fbbd
clivercolongne <clivercolongne@gmail.com>
2022-02-28 18:12:51 -0300

    actualizacion

---
+++ libs/options.js
@@ -0,0 +1,11 @@
+const options = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'Huawei123',
+        database: 'ecommerce'
+    }
+}
+
+module.exports = { options }
